9a662483f0deebc05e328909dbd353066f5d247d
rnema19 <nemarishabh9@gmail.com>
2024-07-19 00:51:20 +0530

    Enter

--- Auth/index.js
+++ Auth/index.js
@@ -26,7 +26,13 @@ app.get('/register',(req,res)=>{
 app.post('/register',async (req,res)=>{
     const {password,username} = req.body
     const hash = await bcrypt.hash(password,12)
-    res.send(hash)
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save()
+    // res.send(hash)
+    res.redirect('/')
 })
 
 app.get('/secret',(req,res)=>{
